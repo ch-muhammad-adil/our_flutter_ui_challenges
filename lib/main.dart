cf6de28e7d3fa7a137036697bbc052c60d60cea7
Adil <chmuhammadadil7@gmail.com>
2019-02-07 12:35:18 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scope Model Learning',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          title: Text('Flutter UI Challenges'),
+          backgroundColor: Colors.green,
+          expandedHeight: 200.0,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              'assets/desert.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
